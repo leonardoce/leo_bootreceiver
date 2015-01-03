acccfd62f9af1f95e14f89b500935ae89f200c78
Leonardo Cecchi <leonardoce@interfree.it>
2015-01-03 15:06:01 +0100

    iniziata la gestione degli allarmi

---
+++ android/src/it/interfree/leonardoce/bootreceiver/AlarmListener.java
@@ -0,0 +1,25 @@
+/**
+ * benCoding.AlarmManager Project
+ * Copyright (c) 2009-2012 by Ben Bahrenburg. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package it.interfree.leonardoce.bootreceiver;
+
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.kroll.common.Log;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AlarmListener  extends BroadcastReceiver {
+	private static final String LTAG = "LeoAlarmListener";
+	@Override 
+	public void onReceive(Context context, Intent intent) {
+		Log.d(LTAG, "Ricevo l'allarme");
+	}
+}
