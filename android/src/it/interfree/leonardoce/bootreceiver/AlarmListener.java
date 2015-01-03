e044eebc50fa14a8b42c63342117d726fcfda716
Leonardo Cecchi <leonardoce@interfree.it>
2015-01-03 16:34:15 +0100

    suona e apre la app

--- android/src/it/interfree/leonardoce/bootreceiver/AlarmListener.java
+++ android/src/it/interfree/leonardoce/bootreceiver/AlarmListener.java
@@ -12,6 +12,7 @@ import org.appcelerator.kroll.common.Log;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,9 +23,26 @@ public class AlarmListener  extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Log.d(LTAG, "Ricevo l'allarme");
 
+		// Questo deve accendere il cellulare
+        AlarmAlertWakeLock.acquireCpuWakeLock(context);
+
         // Play the alarm alert and vibrate the device.
         Log.d(LTAG, "Attivo il servizio...");
         Intent playAlarm = new Intent(context, AlarmKlaxon.class);
         context.startService(playAlarm);
+
+        // Apri l'applicazione
+        Log.d(LTAG, "Attivo l'applicazione...");
+        Intent intentApplicazione = new Intent();
+        intentApplicazione.setComponent(new ComponentName(TiApplication.getInstance().getApplicationContext().getPackageName(), 
+        	"it.interfree.leonardoce.memofarma.MemofarmaActivity"));
+        intentApplicazione.addFlags(
+        	Intent.FLAG_ACTIVITY_NEW_TASK | 
+        	Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | 
+        	Intent.FLAG_ACTIVITY_SINGLE_TOP |
+        	Intent.FLAG_ACTIVITY_NO_USER_ACTION);
+    	intentApplicazione.addCategory(Intent.CATEGORY_LAUNCHER);
+    	intentApplicazione.putExtra("tipologia", "terapie_non_somministrate");
+    	context.startActivity(intentApplicazione);
 	}
 }
